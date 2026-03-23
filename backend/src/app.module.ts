0c8c332dda51b4b6b5cdc273c2142ecfc9c03118
StevenAngel <steven.engel1511@gmail.com>
2026-03-23 20:31:57 +0100

    Add basic resources for project, such as controller, module, service, entity, also setup postgres DB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectsModule } from './projects/projects.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'ToDoApp',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ProjectsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
