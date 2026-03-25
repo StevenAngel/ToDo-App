1d390efa2784adb311a1f71951bccbf214ec6122
StevenAngel <steven.engel1511@gmail.com>
2026-03-25 19:09:21 +0100

    Add open dialog functionality and comments

--- frontend/src/components/dashboard/DashboardDisplay.vue
+++ frontend/src/components/dashboard/DashboardDisplay.vue
@@ -2,6 +2,7 @@
     <v-container>
         <v-card hover @click="load = !load">
             <v-card-item>
+                <!-- vuetify hat in manchen elementen "slots" die man füllen kann, dies macht man hauptsächlich mit <template>. v-slot:prepend oder kurz (#prepend) sagt vuetify, dass dieses element vorne angehängt werden soll.   -->
                 <template #prepend>
                     <v-icon icon="mdi-folder" />
                 </template>
@@ -25,14 +26,17 @@
                     <v-card-actions>
                         <v-spacer></v-spacer>
                         <v-dialog>
-                            <template #activator>
-                                <v-btn variant="tonal" color="red">
+                            <!-- v-slot (#activator) ist das Element, dass den Dialog öffnen soll. Es legt für den v-dialog ein object props an. Es ist ein eventListener wie onClick.  -->
+                            <template #activator="{props}">
+                                <!-- mit v-bind binde ich das object an den button, damit vuetify weiß, es muss den dialog onclick öffnen  -->
+                                <v-btn variant="tonal" color="red" v-bind="props">
                                     Delete
                                 </v-btn>
                             </template>
-
+                            <template #default="{ isActive }">
+                                <p>test</p>
+                            </template>
                         </v-dialog>
-
                     </v-card-actions>
                 </div>
             </v-expand-transition>
@@ -43,5 +47,6 @@
 import { ref, type Ref } from 'vue';
 
 const showProject: Ref<boolean> = ref(false)
+const isActive: Ref<boolean> = ref(false)
 const load: Ref<boolean> = ref(false) // Placeholder to load project site later
 </script>
