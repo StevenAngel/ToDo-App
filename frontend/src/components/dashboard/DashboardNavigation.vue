54439590a7bddd130af1ec7e0bc19eb31ed71b9d
StevenAngel <steven.engel1511@gmail.com>
2026-03-25 17:39:08 +0100

    Add simple dashboard navigation

---
+++ frontend/src/components/dashboard/DashboardNavigation.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container>
+        <v-navigation-drawer permanent v-col-auto>
+            <v-list-item link prepend-icon="mdi-view-dashboard" title="Dashboard"></v-list-item>
+            <v-list-item link prepend-icon="mdi-folder" title="Projects"></v-list-item>
+            <v-container>
+                <v-btn block> Test</v-btn>
+            </v-container>
+        </v-navigation-drawer>
+    </v-container>
+</template>
+
+<script lang="ts" setup>
+</script>
