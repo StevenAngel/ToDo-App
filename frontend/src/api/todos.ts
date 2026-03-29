74e814429b00cd3b5c2704c476091dc8af256f60
StevenAngel <steven.engel1511@gmail.com>
2026-03-29 17:37:48 +0200

    Change todo API

--- frontend/src/api/todos.ts
+++ frontend/src/api/todos.ts
@@ -3,7 +3,7 @@ import type { Todo, CreateTodo, UpdateTodo } from "@/types/todo.ts";
 
 export const todoApi = {
     create: (todo: CreateTodo) => api.post<CreateTodo>(`/todos`, todo),
-    getAll: () => api.get<Todo[]>('/todos'),
+    getAll: (projectId: string) => api.get<Todo[]>(`/todos/project/${projectId}`),
     getById: (id: string) => api.get<Todo>(`/todos/${id}`),
     update: (id: string, todo: UpdateTodo) => api.patch<UpdateTodo>(`/todos/${id}`, todo),
     delete: (id: string) => api.delete<Todo>(`/todos/${id}`)
