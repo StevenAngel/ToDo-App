34eea3447f50dd56870b4435966459a7c814bef8
StevenAngel <steven.engel1511@gmail.com>
2026-03-29 15:57:51 +0200

    Add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Zur Daten validierung (eingestellt in den dtos)
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:8080'
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
