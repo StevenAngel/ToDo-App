f4bfbbcd45576f24a23108b0fb4163cfefb378fc
StevenAngel <steven.engel1511@gmail.com>
2026-05-03 11:34:56 +0200

    Add Navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <v-app>
-    <router-view></router-view>
+    <v-layout>
+      <Navbar />
+      <router-view></router-view>
+    </v-layout>
+
   </v-app>
 </template>
 
 <script lang="ts" setup>
-
+import Navbar from './components/ui/Navbar.vue';
 </script>
---
+++ frontend/src/components/ui/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app-bar class="ga-2">
+        <v-app-bar-title text="ToDo Planner" class="flex-0-0 mr-4"></v-app-bar-title>
+        <div class="d-flex ga-2">
+            <v-btn variant="text" to="/">Home</v-btn>
+            <v-btn variant="text" to="/projects">Home</v-btn>
+        </div>
+    </v-app-bar>
+</template>
+<script lang="ts" setup>
+
+</script>
+<style scoped></style>
