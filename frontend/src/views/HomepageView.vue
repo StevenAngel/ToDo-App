dd391cf1e6b797fd9e2de940b1c1df9bafbe0f4f
StevenAngel <steven.engel1511@gmail.com>
2026-05-03 11:13:31 +0200

    Add Homepage View

--- frontend/src/views/HomepageView.vue
+++ frontend/src/views/HomepageView.vue
@@ -1,7 +1,11 @@
 <template>
     <v-main>
         <v-container>
-            <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+            <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" >
+                <v-container class="d-flex flex-column fill-height justify-center align-center">
+                    <h1 class="font-weight-thin">ToDo Planner</h1>
+                    <h4 class="subheading">This project was created for demonstration and learning purposes only.</h4>
+                </v-container>
             </v-parallax>
         </v-container>
 
